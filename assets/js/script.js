f0b45a752da6af7a8c1c97f5182fdb9bd0f60dde
cincsobrepena <sobrepenacarliannoel@gmail.com>
2023-11-13 22:51:31 +0800

    Add capstone project files

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function Menu(e){
+    let list = document.querySelector('ul');
+    console.log(list);
+    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]') , list.classList.add('opacity-100'), list.classList.add('placeOnTop')): (e.name = "menu", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100') , list.classList.remove('placeOnTop'))
+}
